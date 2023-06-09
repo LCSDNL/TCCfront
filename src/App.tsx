e11e07d5ce2eddad398a0a87e7f631245ac2b141
LCSDNL <lucasdanielrambo@gmail.com>
2023-06-08 23:36:28 -0300

    chamada API e retorno exibido me tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [perguntatxt, setPerguntatxt] = useState("");
-  const [respostatxt, setRespostatxt] = useState("");
+  const [perguntatxt, setPerguntatxt] = useState('');
+  const [respostatxt, setRespostatxt] = useState('');
 
-  function AskService(text: string){
+  function AskService() {
+    const requestData = {
+      pergunta: perguntatxt
+    };
 
-    setRespostatxt(text)
-    return(
-        <>
-        {respostatxt}
-        </>
-    )
-}
+    axios
+      .post('http://localhost:3030/resposta', requestData)
+      .then((res) => {
+        setRespostatxt(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-
-    return (
+  return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Faça sua pergunta.
-      </h1>
+      <h1 className="text-3xl font-bold underline">Faça sua pergunta.</h1>
       <div className="mt-2">
-                <textarea onChange={e=>setPerguntatxt(e.target.value)}
-                  id="pergunta"
-                  name="pergunta"
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
+        <textarea
+          onChange={(e) => setPerguntatxt(e.target.value)}
+          id="pergunta"
+          name="pergunta"
+          rows={3}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          defaultValue={''}
+        />
       </div>
       <div>
-        <button
-        onClick={()=>{ AskService(perguntatxt)}}
-        >
-          Enviar
-        </button>
+        <button onClick={AskService}>Enviar</button>
       </div>
-      
-      <h1 className="text-3xl font-bold underline">
-      {respostatxt.length==0 ? 'Agurdando Pergunta': respostatxt}
-      </h1>
 
-   
+      <h1 className="text-3xl font-bold underline">
+        {respostatxt.length === 0 ? 'Aguardando Pergunta' : respostatxt}
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
